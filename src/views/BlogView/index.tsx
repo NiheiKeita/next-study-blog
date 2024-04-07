cb97bd688df8bbe8291047c7e7df4b85245f05d8
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2024-04-07 23:10:16 +0900

    appルータ修正

--- src/views/BlogView/index.tsx
+++ src/views/BlogView/index.tsx
@@ -5,11 +5,6 @@ import { BlogList } from "@/components/BlogList";
 import { NotionPost } from "@/types/NotionPost";
 
 export async function BlogView() {
-  // var posts: NotionPost[];
-  // const [posts, setPosts] = useState<NotionPost>();
-  // useEffect( () => {
-  //   setPosts(getAllPosts());
-  // }, []);
   const posts: NotionPost[] = await getAllPosts();
   
   return (
@@ -18,19 +13,3 @@ export async function BlogView() {
     </div>
   );
 };
-
-
-
-// export default function BlogView() {
-//   return <div>HOME</div>;
-// export const revalidate = 60;
-
-// export default async function BlogView() {
-//   const posts: NotionPost[] = await getAllPosts();
-
-//   return (
-//     <div className="flex flex-col p-8 lg:w-4/6 mx-auto items-center">
-//       <BlogList notionPosts={posts} />
-//     </div>
-//   );
-// }
